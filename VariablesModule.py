86d016c1f9408bc614a6e31d0c46c6e793cb247a
jpedrocm <jpcm@cin.ufpe.br>
2017-06-06 05:28:12 -0300

    Rolling back variables

--- VariablesModule.py
+++ VariablesModule.py
@@ -6,32 +6,44 @@ from sklearn.linear_model import LogisticRegression, SGDClassifier
 """This module contains all variables for experimentation and prediction."""
 
 
+# NUMBER OF FOLDS IN CROSS VALIDATION
 N_FOLDS = 5
 
-ROWS_REMOVABLES_ALL = ['n_accounts', 'n_issues', 'n_bankruptcies', 'ok_since',
+# DICT OF (MODEL_INITIAL: MODEL OBJECT)
+MODEL_DICT = {"lg": LogisticRegression(n_jobs = -1, random_state = 14128), 
+              "sgd": SGDClassifier(n_jobs = -1, random_state = 14128),
+              "rf": RandomForestClassifier(n_jobs = -1, random_state = 14128)}
+
+# ROWS CONTAINING MISSING VALUES IN ALL OF THESE COLUMNS WILL BE REMOVED
+ROWS_REMOVABLE_ALL = ['n_accounts', 'n_issues', 'n_bankruptcies', 'ok_since',
                    'n_defaulted_loans', 'real_state', 'job_name']
 
-ROWS_REMOVABLES_ANY = ['state', 'zip', 'last_payment', 'end_last_loan']
+# ROWS CONTAINING MISSING VALUES IN ANY OF THESE COLUMNS WILL BE REMOVED
+ROWS_REMOVABLE_ANY = ['state', 'zip', 'last_payment', 'end_last_loan']
 
-HEADERS_REMOVALBLE = ['channel', 'sign', 'ok_since', 'job_name']
+# COLUMNS TO BE REMOVED
+HEADERS_REMOVALBLE = ['channel', 'sign', 'ok_since', 'job_name', 'reason',
+                      'last_payment', 'end_last_loan']
 
+# COLUMNS TO IMPUTE MISSING VALUES WITH MEAN
 HEADERS_MEAN = ['risk_rate', 'income', 'score_3', 'score_4', 'score_5',
                 'score_6']
 
+# COLUMNS TO IMPUTE MISSING VALUES WITH MEDIAN
 HEADERS_MEDIAN = ['credit_limit', 'amount_borrowed']
 
+# COLUMNS TO IMPUTE MISSING VALUES WITH MODE
 HEADERS_MODE = ['default', 'n_bankruptcies', 'n_defaulted_loans', 
                 'n_accounts', 'n_issues', 'real_state', 'borrowed_in_months',
                 'score_1', 'score_2']
 
-HEADERS_PREVIOUS = ['facebook_profile', 'gender', 'reason', 'state', 'zip']
+# COLUMNS TO IMPUTE MISSING VALUES WITH SOME VALUE IN THE DISTRIBUTION
+HEADERS_PREVIOUS = ['facebook_profile', 'gender', 'state', 'zip']
     #last paymnt, end last loan
 
+# COLUMNS TO ENCODE FROM BOOLEANS TO 1's AND 0's
 HEADERS_BOOLEAN = ['facebook_profile', 'default']
 
-HEADERS_CATEGORICAL = ['gender', 'real_state', 'score_1', 'score_2', 'reason', 
+# COLUMNS TO ONE-HOT ENCODE
+HEADERS_CATEGORICAL = ['gender', 'real_state', 'score_1', 'score_2', 
                        'state','zip']
-
-MODEL_DICT = {"lg": LogisticRegression(n_jobs = -1, random_state = 14128), 
-              "sgd": SGDClassifier(n_jobs = -1, random_state = 14128),
-              "rf": RandomForestClassifier(n_jobs = -1, random_state = 14128)}
